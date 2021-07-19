d9882992a253d9607a215464ba0342a5cf72f749
Taqi Raza <mttalpur@gmail.com>
2021-07-20 01:03:39 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cnctsuns () {
+
 # Convert and copy icon which is needed for desktop integration into place:
 wget -q https://github.com/mmtrt/cnctsun/raw/master/snap/gui/cnctsun.png &>/dev/null
 for width in 8 16 22 24 32 36 42 48 64 72 96 128 192 256; do
@@ -11,8 +13,6 @@ done
 wget -qc "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod +x ./appimagetool-x86_64.AppImage
 ./appimagetool-x86_64.AppImage --appimage-extract &>/dev/null
-
-cnctsuns () {
 
 mkdir -p ts-mp/usr ts-mp/winedata ; cp cnctsun.desktop ts-mp ; cp AppRun ts-mp ;
 cp -r icons ts-mp/usr/share ; cp cnctsun.png ts-mp
@@ -30,23 +30,6 @@ export ARCH=x86_64; squashfs-root/AppRun -v ./ts-mp -u "gh-releases-zsync|mmtrt|
 
 cnctsunswp () {
 
-sudo dpkg --add-architecture i386
-wget -qnc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add winehq.key &>/dev/null
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' &>/dev/null
-sudo apt update &>/dev/null
-sudo apt install --install-recommends winehq-stable -y &>/dev/null ; sudo apt install cabextract -y &>/dev/null
-wget -qnc https://github.com/Winetricks/winetricks/raw/master/src/winetricks
-chmod +x winetricks ; sudo mv winetricks /usr/bin
-
-# Disable FileOpenAssociations
-sudo sed -i 's|    LicenseInformation|    LicenseInformation,\\\n    FileOpenAssociations|g;$a \\n[FileOpenAssociations]\nHKCU,Software\\Wine\\FileOpenAssociations,"Enable",,"N"' /opt/wine-stable/share/wine/wine.inf
-# Disable winemenubuilder
-sudo sed -i 's|    FileOpenAssociations|    FileOpenAssociations,\\\n    DllOverrides|;$a \\n[DllOverrides]\nHKCU,Software\\Wine\\DllOverrides,"*winemenubuilder.exe",,""' /opt/wine-stable/share/wine/wine.inf
-sudo sed -i '/\%11\%\\winemenubuilder.exe -a -r/d' /opt/wine-stable/share/wine/wine.inf
-# Pre patching DPI setting DPI dword value 240=f0 180=b4 120=78 110=6e 100=64 96=60
-sudo sed -i 's|0x00000060|0x00000064|' /opt/wine-stable/share/wine/wine.inf
-
 export WINEDLLOVERRIDES="mshtml="
 export WINEARCH="win32"
 export WINEPREFIX="/home/runner/.wine"
@@ -54,15 +37,19 @@ export WINEDEBUG="-all"
 
 cnctsuns ; rm ./*AppImage*
 
+WINE_VER="$(wget -qO- https://dl.winehq.org/wine-builds/ubuntu/dists/focal/main/binary-i386/ | grep wine-stable | sed 's|_| |g;s|~| |g' | awk '{print $5}' | tail -n1)"
+wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous/wine-stable_${WINE_VER}-x86_64.AppImage
+chmod +x *.AppImage ; mv wine-stable_${WINE_VER}-x86_64.AppImage wine-stable.AppImage
+
 # Create winetricks & wine cache
 mkdir -p /home/runner/.cache/{wine,winetricks}/{dotnet20,ahk} ; cp dotnetfx.exe /home/runner/.cache/winetricks/dotnet20
 cp -Rp ./*.msi /home/runner/.cache/wine/ ; cp -Rp AutoHotkey104805_Install.exe /home/runner/.cache/winetricks/ahk
 
 # Create WINEPREFIX
-winetricks -q dotnet20 ; sleep 5
+./wine-stable.AppImage winetricks -q dotnet20 ; sleep 5
 
 # Install game
-(wine TiberianSun_Online_Installer.exe /silent ; sleep 10 ; wineserver -k)
+( ./wine-stable.AppImage wine TiberianSun_Online_Installer.exe /silent ; sleep 10 ; wineserver -k)
 
 # Download game updates manually
 for pkgs in CnCNet5Version.txt cncnet5.7z Sounds.7z Language.7z Icons.7z GeoIP.7z ts-spawn.7z TS_Maps.7z TS_Rules.7z TS_CnCNet5ClientBackground.7z System.Data.SQLite.dll.7z hints.7z LAN.7z _Servers.7z ts-voxels.7z ts-config.7z ts-terrain.7z; do
@@ -85,11 +72,11 @@ mkdir -p "tmp/Maps/Balance-Vet Patch" "tmp/Maps/Popular Mods"
 wget -qO- "https://files.cncnet.org/maps.php?game=ts" > "tmp/CnCNet5/Others/TiberianSunMaps.ini"
 
 for pkgs in $(wget -qO- "https://gist.github.com/mmtrt/449d0f655b0673b55b7723826267e06e/raw/36d5e557cd24b0e750040df7235ce50172e717f1/mapbvt.txt"); do
-(cd "tmp/Maps/Balance-Vet Patch" || exit ; wget -q "https://mapdb.cncnet.org/ts/$pkgs.zip" ; 7z x "$pkgs".zip &>/dev/null ; rm "$pkgs".zip)
+( cd "tmp/Maps/Balance-Vet Patch" || exit ; wget -q "https://mapdb.cncnet.org/ts/$pkgs.zip" ; 7z x "$pkgs".zip &>/dev/null ; rm "$pkgs".zip )
 done
 
 for pkgs in $(wget -qO- "https://files.cncnet.org/maps.php?game=ts" | awk '/Popular/,EOF' | sed -r '/Training/q;1d' | head -n -3 | cut -d'=' -f1); do
-(cd "tmp/Maps/Popular Mods" || exit ; wget -q "https://mapdb.cncnet.org/ts/$pkgs.zip" ; 7z x "$pkgs".zip &>/dev/null ; rm "$pkgs".zip)
+( cd "tmp/Maps/Popular Mods" || exit ; wget -q "https://mapdb.cncnet.org/ts/$pkgs.zip" ; 7z x "$pkgs".zip &>/dev/null ; rm "$pkgs".zip )
 done
 
 cp -Rp tmp/* TiberianSun_Online/ ; rm ./*.7z
@@ -98,7 +85,7 @@ cp -Rp ./TiberianSun_Online "$WINEPREFIX"/drive_c/
 # Removing any existing user data
 ( cd "$WINEPREFIX/drive_c/" ; rm -rf users ) || true
 
-cp -Rp $WINEPREFIX ts-mp/ ; rm -rf $WINEPREFIX ; rm -rf ./ts-mp/winedata
+cp -Rp $WINEPREFIX ts-mp/ ; rm -rf $WINEPREFIX ; rm -rf ./ts-mp/winedata ; rm ./*.AppImage
 
 ( cd ts-mp || exit ; wget -qO- 'https://gist.github.com/mmtrt/49df9fc50ae567a3d5d89791bdb65d45/raw/19b5f3a08fa3ec1429e989adba1cfe9314cd6b52/cnctsunswp.patch' | patch -p1 )
 
