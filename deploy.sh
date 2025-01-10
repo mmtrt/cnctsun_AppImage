9954626201fb330304c9eeecb9f8392e25ce8c50
Taqi Raza <mttalpur@gmail.com>
2025-01-10 15:08:12 +0500

    Update AppImage for dotnet and v7 tsun client

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,28 @@
 
 cnctsuns () {
 
-wget -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage" -O builder ; chmod +x builder
+wget -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage" -O builder ; chmod +x builder ; ./builder --appimage-extract &>/dev/null
 
+# add custom mksquashfs
+wget -q "https://github.com/mmtrt/WINE_AppImage/raw/master/runtime/mksquashfs" -O squashfs-root/usr/bin/mksquashfs
+
+# force zstd format in appimagebuilder for appimages
+rm builder ; sed -i 's|xz|zstd|' squashfs-root/usr/lib/python3.8/site-packages/appimagebuilder/modules/prime/appimage_primer.py
+
+# Add static appimage runtime
+mkdir -p appimage-build/prime AppDir/winedata
+wget -q "https://github.com/mmtrt/WINE_AppImage/raw/master/runtime/runtime-x86_64" -O appimage-build/prime/runtime-x86_64
 mkdir -p ts-mp/usr/share/icons ts-mp/winedata ; cp cnctsun.desktop ts-mp ; cp wrapper ts-mp ; cp cnctsun.png ts-mp/usr/share/icons
 
-TS_VERSION=6.$(wget -q -O- https://www.moddb.com/mods/tiberian-sun-client/downloads/tiberian-sun-client-600 | grep TS_Client | cut -d'.' -f2)
+wget -q "https://github.com/mmtrt/dotnet-runtime_AppImage/releases/download/continuous/dotnet-runtime-$(wget -qO- https://github.com/mmtrt/dotnet-runtime_AppImage/releases/expanded_assets/continuous | grep -Eo me-.* | tail -1 | sed 's|-| |g' | awk '{print $2}')-x86_64.AppImage" -O AppDir/winedata/dotnet ; chmod +x AppDir/winedata/dotnet
 
-wget -q "https://dl.winehq.org/wine/wine-mono/4.7.5/wine-mono-4.7.5.msi"
-wget -q $(wget -q -O- https://www.moddb.com/downloads/"$(wget -q -O- "https://www.moddb.com/mods/tiberian-sun-client/downloads/tiberian-sun-client-600" |grep -Eo "/start/.*" | cut -d'"' -f1)" | grep -Eo https.* | grep mirror | cut -d'"' -f1) -O tsclient.zip
-wget -q "https://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe"
-wget -q "https://web.archive.org/web/20120325002813/https://download.microsoft.com/download/A/C/2/AC2C903B-E6E8-42C2-9FD7-BEBAC362A930/xnafx40_redist.msi"
+TS_VERSION=7.$(wget -q -O- https://www.moddb.com/mods/tiberian-sun-client/downloads/tsclient70 | grep TSClient | cut -d'.' -f2)
 
-cp -Rp ./*.exe ts-mp/winedata ; cp -Rp ./*.msi ts-mp/winedata
+wget -q $(wget -q -O- https://www.moddb.com/downloads/"$(wget -q -O- "https://www.moddb.com/mods/tiberian-sun-client/downloads/tsclient70" |grep -Eo "/start/.*" | cut -d'"' -f1)" | grep -Eo https.* | grep mirror | cut -d'"' -f1) -O tsclient.zip
+
+wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous-stable/wine-stable_$(wget -qO- https://github.com/mmtrt/WINE_AppImage/releases/expanded_assets/continuous-stable | grep -Eo 'stable_[0-9].*' | cut -d'_' -f2 | cut -d'-' -f1 | head -1)-x86_64.AppImage -O wine-stable.AppImage
+chmod +x *.AppImage ; cp wine-stable.AppImage ts-mp/winedata/
+
 sed -i -e 's|progVer=|progVer='"$TS_VERSION"'|g' ts-mp/wrapper
 
 mkdir -p AppDir/winedata AppDir/usr/share/tsclient ; cp -r "ts-mp/"* AppDir
@@ -23,37 +33,47 @@ unzip tsclient.zip -d AppDir/usr/share/tsclient
 
 # sed -i "s|520|$NVDV|" cnctsun.yml
 
-./builder --recipe cnctsun.yml
+chmod +x AppDir/usr/share/tsclient/wine-ts.sh
+
+./squashfs-root/AppRun --recipe cnctsun.yml
 
 }
 
 cnctsunswp () {
 
-export WINEDLLOVERRIDES="mshtml="
-export WINEARCH="win32"
+export WINEDLLOVERRIDES="mscoree,mshtml="
 export WINEPREFIX="/home/runner/work/cnctsun_AppImage/cnctsun_AppImage/AppDir/winedata/.wine"
 export WINEDEBUG="-all"
 
-wget -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage" -O builder ; chmod +x builder
+wget -q "https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.0.3/appimage-builder-1.0.3-x86_64.AppImage" -O builder ; chmod +x builder ; ./builder --appimage-extract &>/dev/null
+
+# add custom mksquashfs
+wget -q "https://github.com/mmtrt/WINE_AppImage/raw/master/runtime/mksquashfs" -O squashfs-root/usr/bin/mksquashfs
+
+# force zstd format in appimagebuilder for appimages
+rm builder ; sed -i 's|xz|zstd|' squashfs-root/usr/lib/python3.8/site-packages/appimagebuilder/modules/prime/appimage_primer.py
+
+# Add static appimage runtime
+mkdir -p appimage-build/prime AppDir/winedata
+wget -q "https://github.com/mmtrt/WINE_AppImage/raw/master/runtime/runtime-x86_64" -O appimage-build/prime/runtime-x86_64
 
 mkdir -p ts-mp/usr/share/icons ts-mp/winedata ; cp cnctsun.desktop ts-mp ; cp wrapper ts-mp ; cp cnctsun.png ts-mp/usr/share/icons
 
-TS_VERSION=6.$(wget -q -O- https://www.moddb.com/mods/tiberian-sun-client/downloads/tiberian-sun-client-600 | grep TS_Client | cut -d'.' -f2)
+wget -q "https://github.com/mmtrt/dotnet-runtime_AppImage/releases/download/continuous/dotnet-runtime-$(wget -qO- https://github.com/mmtrt/dotnet-runtime_AppImage/releases/expanded_assets/continuous | grep -Eo me-.* | tail -1 | sed 's|-| |g' | awk '{print $2}')-x86_64.AppImage" -O AppDir/winedata/dotnet ; chmod +x AppDir/winedata/dotnet
 
-wget -q $(wget -q -O- https://www.moddb.com/downloads/"$(wget -q -O- "https://www.moddb.com/mods/tiberian-sun-client/downloads/tiberian-sun-client-600" |grep -Eo "/start/.*" | cut -d'"' -f1)" | grep -Eo https.* | grep mirror | cut -d'"' -f1) -O tsclient.zip
-wget -q "https://download.microsoft.com/download/9/5/A/95A9616B-7A37-4AF6-BC36-D6EA96C8DAAE/dotNetFx40_Full_x86_x64.exe"
-wget -q "https://web.archive.org/web/20120325002813/https://download.microsoft.com/download/A/C/2/AC2C903B-E6E8-42C2-9FD7-BEBAC362A930/xnafx40_redist.msi"
+TS_VERSION=7.$(wget -q -O- https://www.moddb.com/mods/tiberian-sun-client/downloads/tsclient70 | grep TSClient | cut -d'.' -f2)
 
-wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous-stable-4-i386/wine-stable-i386_4.0.4-x86_64.AppImage
-chmod +x *.AppImage ; mv wine-stable-i386_4.0.4-x86_64.AppImage wine-stable.AppImage
+wget -q $(wget -q -O- https://www.moddb.com/downloads/"$(wget -q -O- "https://www.moddb.com/mods/tiberian-sun-client/downloads/tsclient70" |grep -Eo "/start/.*" | cut -d'"' -f1)" | grep -Eo https.* | grep mirror | cut -d'"' -f1) -O tsclient.zip
+
+wget -q https://github.com/mmtrt/WINE_AppImage/releases/download/continuous-stable/wine-stable_$(wget -qO- https://github.com/mmtrt/WINE_AppImage/releases/expanded_assets/continuous-stable | grep -Eo 'stable_[0-9].*' | cut -d'_' -f2 | cut -d'-' -f1 | head -1)-x86_64.AppImage -O wine-stable.AppImage
+chmod +x *.AppImage ; cp wine-stable.AppImage ts-mp/winedata/
 
 # Create winetricks & wine cache
-mkdir -p /home/runner/.cache/{wine,winetricks}/{dotnet40,ahk,xna40} AppDir/usr/share/tsclient ; cp dotNetFx40_Full_x86_x64.exe /home/runner/.cache/winetricks/dotnet40 ; cp xnafx40_redist.msi /home/runner/.cache/winetricks/xna40
 rm wrapper
 
 # Create WINEPREFIX
-mkdir -p "$WINEPREFIX/drive_c/windows/assembly"
-./wine-stable.AppImage winetricks -q xna40 vcrun2010; sleep 5
+./wine-stable.AppImage winecfg &
+sleep 2 ; pkill winecfg
 
 unzip tsclient.zip -d AppDir/usr/share/tsclient
 
@@ -70,11 +90,13 @@ echo "disabled" > $WINEPREFIX/.update-timestamp ; cp -r "ts-mp/"* AppDir
 
 # sed -i "s|520|$NVDV|" cnctsun.yml
 
+chmod +x AppDir/usr/share/tsclient/wine-ts.sh
+
 sed -i -e 's|progVer=|progVer='"${TS_VERSION}_WP"'|g' AppDir/wrapper
 
-sed -i 's/stable-v6|/stable-wp-v6|/' cnctsun.yml
+sed -i 's/stable-v7|/stable-wp-v7|/' cnctsun.yml
 
-./builder --recipe cnctsun.yml
+./squashfs-root/AppRun --recipe cnctsun.yml
 
 }
 
