66422ab239cf399ddbb7e1c8e19ba3e20fd41d70
Taqi Raza <mttalpur@gmail.com>
2026-02-07 11:07:51 +0500

    update `wine-ts.sh`

--- wine-ts.sh
+++ wine-ts.sh
@@ -3,26 +3,26 @@
 progWINE=cnctsun-x.xx-x86_64.AppImage
 progBin=game.exe
 
-if command -v "$OWD/$progWINE" > /dev/null 2>&1; then
-"$OWD/$progWINE" "$progBin" "$*"
+if command -v "$OWD/$progWINE" > /dev/null 2>&1  ; then
+"$OWD/$progWINE" "$progBin" -SPAWN "$*"
 fi
 
-if command -v "$HOME/Downloads/$progWINE" > /dev/null 2>&1; then
-"$HOME/Downloads/$progWINE" "$progBin" "$*"
+if command -v "$HOME/Downloads/$progWINE" > /dev/null 2>&1  ; then
+"$HOME/Downloads/$progWINE" "$progBin" -SPAWN "$*"
 fi
 
-if command -v "$HOME/$progWINE" > /dev/null 2>&1; then
-"$HOME/$progWINE" "$progBin" "$*"
+if command -v "$HOME/$progWINE" > /dev/null 2>&1 ; then
+"$HOME/$progWINE" "$progBin" -SPAWN "$*"
 fi
 
-if command -v "$HOME/Desktop/$progWINE" > /dev/null 2>&1; then
-"$HOME/Desktop/$progWINE" "$progBin" "$*"
+if command -v "$HOME/Desktop/$progWINE" > /dev/null 2>&1 ; then
+"$HOME/Desktop/$progWINE" "$progBin" -SPAWN "$*"
 fi
 
-if command -v "$HOME/bin/$progWINE" > /dev/null 2>&1; then
-"$HOME/bin/$progWINE" "$progBin" "$*"
+if command -v "$HOME/bin/$progWINE" > /dev/null 2>&1 ; then
+"$HOME/bin/$progWINE" "$progBin" -SPAWN "$*"
 fi
 
-if command -v "$HOME/.local/bin/$progWINE" > /dev/null 2>&1; then
-"$HOME/.local/bin/$progWINE" "$progBin" "$*"
+if command -v "$HOME/.local/bin/$progWINE" > /dev/null 2>&1 ; then
+"$HOME/.local/bin/$progWINE" "$progBin" -SPAWN "$*"
 fi
